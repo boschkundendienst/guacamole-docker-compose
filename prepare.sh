3ec018b47483fb7eeda91bc33572ae3b89c2d511
Boschkundendienst <boschkundendienst@users.noreply.github.com>
2021-10-11 22:25:50 +0200

    added check for running docker daemon
    added code to check if docker daemon is running since it is needed to run database initialization.

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 #
+# check if docker is running
+if ! (docker ps >/dev/null 2>&1)
+then
+	echo "docker daemon not running, will exit here!"
+	exit
+fi
 echo "Preparing folder init and creating ./init/initdb.sql"
 mkdir ./init >/dev/null 2>&1
 mkdir -p ./nginx/ssl >/dev/null 2>&1

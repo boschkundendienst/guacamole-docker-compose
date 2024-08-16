55c0d7e2278d60d8e2be841bdd19c9fec136ea8c
fragstein84 <gilles.pascal@gmail.com>
2024-08-15 15:50:07 -1000

    + Bug when you want to change the SQL database password in the YML file after the first 'prepare.sh' => Fixed. + 'TOTP' and 'history-recording' extensions. => Added.

--- prepare.sh
+++ prepare.sh
@@ -1,18 +1,60 @@
 #!/bin/sh
+#####################################################################################
+#
+# version            date              comment
+# 0.9                2024/08/15        add TOTP + history-recording and folders
+#
+#####################################################################################
+# version : guacamole 1.5.5
+GUAC_VERSION="1.5.5"
+
 #
 # check if docker is running
 if ! (docker ps >/dev/null 2>&1)
 then
-	echo "docker daemon not running, will exit here!"
+	echo "/!\ docker daemon not running, will exit here!"
 	exit
 fi
-echo "Preparing folder init and creating ./init/initdb.sql"
+
+# Creation folders
+echo "++ Creating folders"
 mkdir ./init >/dev/null 2>&1
 mkdir -p ./nginx/ssl >/dev/null 2>&1
 chmod -R +x ./init
+mkdir ./extensions >/dev/null 2>&1
+mkdir ./recordings >/dev/null 2>&1
+echo "=> done"
+
+# SQL database initialization
+echo "++ Creating ./init/initdb.sql"
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgresql > ./init/initdb.sql
-echo "done"
-echo "Creating SSL certificates"
+echo "=> done"
+
+# Downloading extensions
+echo "++ Download extensions"
+FILE="guacamole-auth-totp-$GUAC_VERSION.tar.gz"
+URL="https://apache.org/dyn/closer.lua/guacamole/$GUAC_VERSION/binary/$FILE"
+LOCAL_PATH="./extensions/$FILE"
+if test -f "$LOCAL_PATH"; then
+    echo "/!\ The $LOCAL_PATH file already exists."
+else
+    echo "/!\ The $LOCAL_PATH file does not exist, download begins..."
+    wget -c -P ./extensions $URL
+fi
+
+FILE="guacamole-history-recording-storage-1.5.5.tar.gz"
+URL="https://apache.org/dyn/closer.lua/guacamole/$GUAC_VERSION/binary/$FILE"
+LOCAL_PATH="./extensions/$FILE"
+if test -f "$LOCAL_PATH"; then
+    echo "/!\ The $LOCAL_PATH file already exists."
+else
+    echo "/!\ The $LOCAL_PATH file does not exist, download begins..."
+    wget -c -P ./extensions $URL
+fi
+echo "=> done"
+
+# Creating certificates
+echo "++ Creating SSL certificates"
 openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=DE/ST=BY/L=Hintertupfing/O=Dorfwirt/OU=Theke/CN=www.createyourown.domain/emailAddress=docker@createyourown.domain'
-echo "You can use your own certificates by placing the private key in nginx/ssl/self-ssl.key and the cert in nginx/ssl/self.cert"
-echo "done"
+echo "/!\ You can use your own certificates by placing the private key in nginx/ssl/self-ssl.key and the cert in nginx/ssl/self.cert"
+echo "=> done"
--- reset.sh
+++ reset.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+#####################################################################################
+#
+# version            date              comment
+# 0.2                2024/08/15        fix SQL base delete when changing base password
+#
+#####################################################################################
 echo "This will delete your existing database (./data/)"
-echo "          delete your recordings        (./record/)"
 echo "          delete your drive files       (./drive/)"
+echo "          delete your recordings        (./recordings/)"
 echo "          delete your certs files       (./nginx/ssl/)"
+echo "          delete your int files         (./init/)"
+echo "          delete your extensions files  (./extensions/)"
 echo ""
-read -p "Are you sure? " -n 1 -r
+read -p "Are you sure? [Y/n]" -n 1 -r
 echo ""   # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]; then # do dangerous stuff
  chmod -R +x -- ./init
- sudo rm -r -f ./data/ ./drive/ ./record/ ./nginx/ssl/
+ sudo rm -r -f ./data/ ./drive/ ./recordings/ ./nginx/ssl/ ./init/ ./extensions/
 fi
-
